974ae36ab19dd028de8912662708d297c38979a8
SushantSankpal <128896348+SushantSankpal@users.noreply.github.com>
2023-03-25 15:47:39 +0530

    Remove unwanted spaces

--- main.py
+++ main.py
@@ -14,6 +14,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print("helloworld"
-      )
+print("helloworld")
 print("New Line")
